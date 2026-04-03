a6dcaff291dbbd42351616aaf959923b30647960
Amarnath Kolla <amarrama269@gmail.com>
2026-04-03 23:04:47 +0530

    Added Snake and Ladder + OOP review questions

---
+++ Oops_Review_question/Oops_Review_question/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+// created class with name Studnet and added the constrcuot and properties
+public class Student
+{
+    public string Name { get; set; } //this is property
+    public long RegId { get; set; }
+    public int Maths { get; set; }
+    public int English { get; set; }
+    public int Science { get; set; }
+
+    // this is the constructor of the class
+    public Student(string name , long regid, int math, int eng, int sci)
+    {
+        Name = name;
+        RegId = regid;
+        Maths = math;
+        English = eng;
+        Science = sci;
+    }
+}
+
+// created the interface with one method for calculating the grade.
+interface IGradecalculator
+{
+    public String Getgrade(double percentage);// method gives which grade student got so used "string"
+
+}
+public abstract class Result
+{
+    protected Student student;
+    public Result(Student s)
+    {
+        student = s;
+    }
+
+    public abstract double CalculateResult();
+}
+
+public class SchoolResult: Result, IGradecalculator
+{
+    public SchoolResult(Student s) : base(s)
+    {
+
+    }
+    public override double CalculateResult()
+    {
+        double total = student.Maths + student.English + student.Science;
+        return total / 3;
+    }
+    public string Getgrade(double percentage)
+    {
+        if (percentage >= 90)
+            return "A";
+        else if (percentage >= 80)
+            return "B";
+        else if (percentage >= 70)
+            return "C";
+        else if (percentage >= 60)
+            return "D";
+        else
+            return "F";
+    }
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+        // object created and passed the values in the constructor.
+        Student s1 = new Student("Amarnath",2211028010157,76,84,90); 
+        SchoolResult result = new SchoolResult(s1);
+        double percentage = result.CalculateResult();
+        String grade = result.Getgrade(percentage);
+        Console.WriteLine("Name : " + s1.Name);
+        Console.WriteLine("RegId : " + s1.RegId);
+        Console.WriteLine($"percentage : {percentage:F2}");
+        Console.WriteLine("Grade : " + grade);
+
+    }
+}
---
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int player1 = 0;
+        int player2 = 0;
+        int diceCount = 0;
+
+        Console.Write("Enter number of players (1 or 2): ");
+        int players = int.Parse(Console.ReadLine());
+
+        int currentPlayer = 1;
+        Random rand = new Random();
+
+        while (true)
+        {
+            Console.WriteLine("\nPlayer " + currentPlayer + " Turn");
+            Console.Write("Press Y to roll dice: ");
+            char ch = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (ch != 'Y')
+            {
+                Console.WriteLine("Invalid input");
+                continue;
+            }
+
+            int dice = rand.Next(1, 7);
+            diceCount++;
+            Console.WriteLine("Dice: " + dice);
+
+            int option = rand.Next(0, 3); // 0 NoPlay, 1 Ladder, 2 Snake
+
+            if (option == 0)
+                Console.WriteLine("No Play");
+            else if (option == 1)
+                Console.WriteLine("Ladder");
+            else
+                Console.WriteLine("Snake");
+
+            // Apply move
+            if (currentPlayer == 1)
+            {
+                player1 = Move(player1, dice, option);
+                Console.WriteLine("Player 1 Position: " + player1);
+
+                if (player1 == 100)
+                {
+                    Console.WriteLine("🎉 Player 1 Wins!");
+                    break;
+                }
+            }
+            else
+            {
+                player2 = Move(player2, dice, option);
+                Console.WriteLine("Player 2 Position: " + player2);
+
+                if (player2 == 100)
+                {
+                    Console.WriteLine("🎉 Player 2 Wins!");
+                    break;
+                }
+            }
+
+            // Change turn (if not ladder)
+            if (option != 1)
+            {
+                if (players == 2)
+                    currentPlayer = (currentPlayer == 1) ? 2 : 1;
+            }
+            else
+            {
+                Console.WriteLine("Extra Turn (Ladder!)");
+            }
+        }
+
+        Console.WriteLine("Total Dice Rolls: " + diceCount);
+    }
+
+    static int Move(int pos, int dice, int option)
+    {
+        if (option == 1) // Ladder
+            pos += dice;
+        else if (option == 2) // Snake
+            pos -= dice;
+
+        if (pos < 0)
+            pos = 0;
+
+        if (pos > 100)
+            pos -= dice;
+
+        return pos;
+    }
+}
