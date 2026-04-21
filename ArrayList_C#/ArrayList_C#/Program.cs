d98cd1fbe2b150373d5a2e34cf9a0c37a770251f
Amarnath Kolla <amarrama269@gmail.com>
2026-04-21 10:31:21 +0530

    arraylist and hashtable

---
+++ ArrayList_C#/ArrayList_C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        Console.WriteLine("Enter the NO of students :");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i =0; i < n; i++)
+        {
+            Console.WriteLine("Enter Your ID : ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Name : ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Marks : ");
+            double marks = double.Parse(Console.ReadLine());
+
+
+            list.Add(id);
+            list.Add(name);
+            list.Add(marks);
+        }
+        Console.WriteLine("The student Data : ");
+        foreach (var item in list)
+        {
+            Console.Write(item);
+
+        }
+
+    }
+    
+    
+    
+}
+
+
+
+
