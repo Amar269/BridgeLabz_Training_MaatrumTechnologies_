e6d4be936b84b0cd14664770602d626d19538ebe
Amarnath Kolla <amarrama269@gmail.com>
2026-05-14 17:28:14 +0530

    updated code using LINQ - QUERY & Mehtod syntax

--- Linq_Basics/Linq_Basics/Program.cs
+++ Linq_Basics/Linq_Basics/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class program
+namespace Linq_basic
 {
-    static void Main(string[] args)
+    class program
     {
-        Console.WriteLine("Hello LINQ");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 4, 5, 5, 7, 9 };
+
+                
+            
+            // this is the query syntax of linq
+                var num =   from obj in numbers
+                             where  obj >= 5
+                             select obj;
+               
+            // this is the method syntax of linq
+            // var num = numbers.where(obj => obj >= 5).ToList();
+      
+            foreach (var n in num)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
     }
 }
+
